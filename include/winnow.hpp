86e87f743fae086ea3f446e0f86d0ac537729ea5
Dhruv <62509177+darkn3to@users.noreply.github.com>
2025-04-05 23:26:33 +0530

    fingerprinting done.

--- include/winnow.hpp
+++ include/winnow.hpp
@@ -1,44 +1,83 @@
 #ifndef WINNOW_HPP
 #define WINNOW_HPP
 
+#define MOD 1000000007
+
 #include "token.hpp"
-#include <climits>
 #include <cstring>
+#include <vector>
+
+using namespace std;
+
+unsigned short* circular_buffer;
+
+unsigned long rhash(const int k, const int rp) {
+    unsigned long h = 0;
+    int idx;
+    for (int i = 0; i < k; i++) {
+        idx = (rp + i) % k;
+        h = (h * 31 + circular_buffer[idx]) % MOD;
+    }
+    return h;
+}
 
 void winnow(const int window, const int k, FILE *file) {
-    unordered_map<int, string> htable;
-    htable.reserve(window);
+    
+    vector<unsigned short> htable;
+    vector<unsigned long> fingerprints;
 
-    int iToken_size = iToken.size();  //iToken is in token.hpp
+    int iToken_size = iToken.size();  // iToken is defined in token.hpp
 
-    unsigned short* circular_buffer = new unsigned short[k];
+    circular_buffer = new unsigned short[k];
 
-    int j = 0;
+    int j = 0, r = 0, min = 0;
     for (int i = 0; i < k && j < iToken_size; i++, j++) {
         circular_buffer[i] = iToken[j];
     }
+
+    htable.push_back(rhash(k, 0));
+    r = (r + 1) % window;
     
-    /*cout << "Initial window: ";
-    for (int i = 0; i < k; i++) {
-        cout << circular_buffer[i] << " ";
-    }
-    cout << endl;
-    */
 
     unsigned short rp = 0;
     
     while (j < iToken_size) {
         circular_buffer[rp] = iToken[j++];
         rp = (rp + 1) % k;
-        /*cout << "Window: ";
-        for (int i = 0; i < k; i++) {
-            int idx = (rp + i) % k;
-            cout << circular_buffer[idx] << " ";
+        if (htable.size() < static_cast<unsigned short>(window)) {
+            htable.push_back(rhash(k, rp));
         }
-        cout << endl;
-        */
+        else {
+            htable[r] = rhash(k, rp);
+        }
+        if (min == r) {
+            for (int i = (r - 1 + window) % window; i != r; i = (i - 1 + window) % window) {
+                if (htable[i] < htable[min]) {
+                    min = i;
+                }
+            }
+            fingerprints.push_back(htable[min]);
+        }
+        else {
+            if (htable[r] < htable[min]) {
+                min = r;
+                fingerprints.push_back(htable[min]);
+            }
+        }
+        
+        /*for (auto it : htable) {
+            cout << it << " ";
+        }
+        cout << endl;*/
+        
+        r = (r + 1) % window;
     }
 
+    for (auto it : fingerprints) {
+        cout << it << " ";
+    } 
+    
+    delete[] circular_buffer;
 }
 
 #endif // WINNOW_HPP
